c5e8764a95bec2f4bdec11a99b881012d37c0cec
DESKTOP-4OK39MV\user <>
2022-10-11 17:28:19 +0900

    [1011] 1st commit

---
+++ src/main/java/com/ming/project/ProjectBookstoreApplication.java
@@ -0,0 +1,16 @@
+package com.ming.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class ProjectBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ming/project/ProjectBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
