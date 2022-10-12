58d916237b4aa01aa0b9e6d8c93d051d6db6ec9f
minjeong073 <kim.mj80730@gmail.com>
2022-10-12 15:33:25 +0900

    [1012] 환경설정
    - jsp, DB 연동
    - minjeong073/SpringProject_bookstore#1 이슈

--- src/main/java/com/ming/project/bookstore/ProjectBookstoreApplication.java
+++ src/main/java/com/ming/project/bookstore/ProjectBookstoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ProjectBookstoreApplication {
 
