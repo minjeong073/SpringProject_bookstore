a5a2419973c0e48eafcf376fda33eee09da9b3ff
DESKTOP-4OK39MV\user <>
2022-11-09 17:23:09 +0900

    [1109] 배포 - aws 배포 코드 적용

--- src/main/java/com/ming/project/bookstore/ProjectBookstoreApplication.java
+++ src/main/java/com/ming/project/bookstore/ProjectBookstoreApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectBookstoreApplication {
+public class ProjectBookstoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBookstoreApplication.class, args);
